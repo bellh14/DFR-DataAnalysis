024f22b22fd1b933bf632029f04edeac3bb56b05
bellh14 <haybell965@gmail.com>
2023-10-17 22:03:54 -0500

    add accelerometer

---
+++ AccelerometerAnalysis.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import plotly.express as px
+import time
+
+
+class AccelerometerAnalaysis:
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        self.xl_data = pd.DataFrame(pd.read_csv(self.file_name))
+        self.x_basis = self.xl_data.loc[0, "X"]
+        self.y_basis = self.xl_data.loc[0, "Y"]
+        self.z_basis = self.xl_data.loc[0, "Z"]
+
+    def convert_time(self, data):
+        for i, row in data.iterrows():
+            time_step = row["Time"]
+            mlsec = repr(time_step).split(".")[1][:3]
+            data.loc[i, "Time"] = time.strftime(
+                "%H:%M:%S.{} %Z".format(mlsec), time.localtime(time_step)
+            )
+
+    def convert_xl_g(self):
+        for i, row in self.xl_data.iterrows():
+            self.xl_data.loc[i, "X"] = (row["X"] - self.x_basis) * 0.53
+            self.xl_data.loc[i, "Y"] = (row["Y"] - self.y_basis) * 0.53
+            self.xl_data.loc[i, "Z"] = (row["Z"] - self.z_basis) * 0.53
+
+    def plot_analog_xl(self, title: str = None):
+        fig = px.line(
+            self.xl_data,
+            x="Time",
+            y=["X", "Y", "Z"],
+            color_discrete_sequence=px.colors.qualitative.Vivid,
+        )
+        fig.update_layout(
+            title=f"Accelerometer Data {title}",
+            yaxis_title="g",
+            xaxis_title="Time",
+            height=1080,
+            width=1920,
+        )
+        fig.show()
+        # fig.write_image(f"10-14/accelerometer_{title}.png")
+
+
+if __name__ == "__main__":
+    # will refactor later
+
+    accel = AccelerometerAnalaysis("output2_analog_2023-10-14_18-28-08.csv")
+    accel.convert_time(accel.xl_data)
+    accel.convert_xl_g()
+    accel.plot_analog_xl("200_Run_1")
+
+    # accel = AccelerometerAnalaysis("output2_analog_2023-10-14_17-15-24.csv")
+    # accel.convert_time(accel.xl_data)
+    # accel.convert_xl_g()
+    # accel.plot_analog_xl("150_Run_2")
+
+    # accel = AccelerometerAnalaysis("output2_analog_2023-10-14_17-17-54.csv")
+    # accel.convert_time(accel.xl_data)
+    # accel.convert_xl_g()
+    # accel.plot_analog_xl("150_Run_3")
--- LinpotAnalysis.py
+++ LinpotAnalysis.py
@@ -5,54 +5,61 @@ from sklearn.cluster import KMeans
 
 
 class LinpotAnalysis:
-
-    def __init__(self, file_name: str, xl_file_name: str):
+    def __init__(self, file_name: str):
         self.file_name = file_name
         self.data = pd.DataFrame(pd.read_csv(self.file_name))
-        self.xl_data = pd.DataFrame(pd.read_csv(xl_file_name))
 
     def convert_voltage_to_mm(self):
         for i, row in self.data.iterrows():
-            self.data.loc[i, 'Front Right'] = row['Front Right'] * 15.0
-            self.data.loc[i, 'Front Left'] = row['Front Left'] * 15.0
-            self.data.loc[i, 'Rear Right'] = row['Rear Right'] * 15.0
-            self.data.loc[i, 'Rear Left'] = row['Rear Left'] * 15.0
+            self.data.loc[i, "Front Right"] = row["Front Right"] * 15.0
+            self.data.loc[i, "Front Left"] = row["Front Left"] * 15.0
+            self.data.loc[i, "Rear Right"] = row["Rear Right"] * 15.0
+            self.data.loc[i, "Rear Left"] = row["Rear Left"] * 15.0
 
     def convert_time(self, data):
         for i, row in data.iterrows():
-            time_step = row['Time']
-            mlsec = repr(time_step).split('.')[1][:3]
-            data.loc[i, 'Time'] = time.strftime("%H:%M:%S.{} %Z".format(mlsec), time.localtime(time_step))
+            time_step = row["Time"]
+            mlsec = repr(time_step).split(".")[1][:3]
+            data.loc[i, "Time"] = time.strftime(
+                "%H:%M:%S.{} %Z".format(mlsec), time.localtime(time_step)
+            )
 
-    def plot(self):
-        fig = px.line(self.data, x='Time', y=['Front Right', 'Front Left',
-                                              'Rear Right', 'Rear Left'], color_discrete_sequence=px.colors.qualitative.Vivid)
-        fig.update_layout(title='Linear Potentiometer Data', yaxis_title='mm', xaxis_title='Time', height=1080, width=1920)
+    def plot(self, title: str = None):
+        fig = px.line(
+            self.data,
+            x="Time",
+            y=["Front Right", "Front Left", "Rear Right", "Rear Left"],
+            color_discrete_sequence=px.colors.qualitative.Vivid,
+        )
+        fig.update_layout(
+            title=f"Linear Potentiometer Data {title}",
+            yaxis_title="mm",
+            xaxis_title="Time",
+            height=1080,
+            width=1920,
+        )
         fig.show()
-        # fig.write_image("saturday_linpot.png")
-
-    def convert_xl_g(self):
-        for i, row in self.xl_data.iterrows():
-            self.xl_data.loc[i, "X"] = row["X"] * 0.5
-            self.xl_data.loc[i, "Y"] = row["Y"] * 0.5
-            self.xl_data.loc[i, "Z"] = row["Z"] * 0.5
+        # fig.write_image(f"10-14/linpot_{title}.png")
 
 
-    def plot_analog_xl(self):
-        fig = px.line(self.xl_data, x='Time', y=["X", "Y", "Z"])
-        # fig.update_layout(title='Linear Potentiometer Data', yaxis_title='mm', xaxis_title='Time', height=1080, width=1920)
-        fig.show()
-
-    def cluster(self):
-        kmeans = KMeans(n_clusters=20)
-        predictions = kmeans.fit(self.data)
-        
-
-if __name__ == '__main__':
-    linpot = LinpotAnalysis('output2_linpot_2023-10-14_18-28-08.csv', "output2_analog_2023-10-14_18-28-08.csv")
+if __name__ == "__main__":
+    linpot = LinpotAnalysis(
+        "output2_linpot_2023-10-14_17-12-10.csv",
+    )
     linpot.convert_voltage_to_mm()
-    linpot.convert_xl_g()
     linpot.convert_time(linpot.data)
-    linpot.convert_time(linpot.xl_data)
-    linpot.plot()
-    linpot.plot_analog_xl()
+    linpot.plot("150_Run_1")
+
+    # linpot = LinpotAnalysis(
+    #     "output2_linpot_2023-10-14_17-15-24.csv",
+    # )
+    # linpot.convert_voltage_to_mm()
+    # linpot.convert_time(linpot.data)
+    # linpot.plot("150_Run_2")
+
+    # linpot = LinpotAnalysis(
+    #     "output2_linpot_2023-10-14_17-17-54.csv",
+    # )
+    # linpot.convert_voltage_to_mm()
+    # linpot.convert_time(linpot.data)
+    # linpot.plot("150_Run_3")

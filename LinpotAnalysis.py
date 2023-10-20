2b1f468f9681417b240f393a0ffe75bc97a7cf63
bellh14 <haybell965@gmail.com>
2023-10-20 00:10:04 -0500

    add low pass filtering

--- LinpotAnalysis.py
+++ LinpotAnalysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import time
 from sklearn.cluster import KMeans
+from Filter import Filter
 
 
 class LinpotAnalysis:
@@ -24,13 +25,39 @@ class LinpotAnalysis:
                 "%H:%M:%S.{} %Z".format(mlsec), time.localtime(time_step)
             )
 
+    def clean_data(self):
+        # self.data = Filter.average(self.data, "Front Right", 25)
+        # self.data = Filter.average(self.data, "Front Left", 25)
+        # self.data = Filter.average(self.data, "Rear Right", 25)
+        # self.data = Filter.average(self.data, "Rear Left", 25)
+        self.data = Filter.butter_lowpass_filter(self.data, "Front Right", 4, 30, 2)
+        self.data = Filter.butter_lowpass_filter(self.data, "Front Left", 4, 30, 2)
+        self.data = Filter.butter_lowpass_filter(self.data, "Rear Right", 4, 30, 2)
+        self.data = Filter.butter_lowpass_filter(self.data, "Rear Left", 4, 30, 2)
+
     def plot(self, title: str = None):
+        # fig = px.line(
+        #     self.data,
+        #     x="Time",
+        #     y=["Front Right", "Front Left", "Rear Right", "Rear Left"],
+        #     color_discrete_sequence=px.colors.qualitative.Vivid,
+        # )
         fig = px.line(
             self.data,
             x="Time",
-            y=["Front Right", "Front Left", "Rear Right", "Rear Left"],
+            y=[
+                "Front Right",
+                "Front Right_lowpass",
+                "Front Left",
+                "Front Left_lowpass",
+                "Rear Right",
+                "Rear Right_lowpass",
+                "Rear Left",
+                "Rear Left_lowpass",
+            ],
             color_discrete_sequence=px.colors.qualitative.Vivid,
         )
+
         fig.update_layout(
             title=f"Linear Potentiometer Data {title}",
             yaxis_title="mm",
@@ -39,16 +66,17 @@ class LinpotAnalysis:
             width=1920,
         )
         fig.show()
-        fig.write_image(f"10-14/linpot_{title}.png")
+        # fig.write_image(f"10-14/linpot_{title}.png")
 
 
 if __name__ == "__main__":
     linpot = LinpotAnalysis(
-        "output2_linpot_2023-10-14_13-23-28.csv",
+        "output2_linpot_2023-10-14_15-28-30.csv",
     )
     linpot.convert_voltage_to_mm()
     linpot.convert_time(linpot.data)
-    linpot.plot("200_Run_1")
+    linpot.clean_data()
+    linpot.plot("150_Run_3")
 
     # linpot = LinpotAnalysis(
     #     "output2_linpot_2023-10-14_17-15-24.csv",

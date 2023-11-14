b4f4f15234a8bc789e2153f8050380635a0ff83f
bellh14 <haybell965@gmail.com>
2023-11-13 20:42:28 -0600

    fix test function to include conversions

--- TestRideHeightFunction.py
+++ TestRideHeightFunction.py
@@ -15,25 +15,34 @@ def plot_ride_heights():
     ride_height_combinations = pd.DataFrame(
         columns=["FRH", "RRH", "Chassis Angle", "Chassis Heave"]
     )
-    num_heaves = 250
-    num_angles = 250
-    heave_increment = (0.5 + abs(-1.2)) / (num_heaves - 1)
-    angle_increment = (1.3 + abs(-1.3)) / (num_angles - 1)
+    num_heaves = 500
+    num_angles = 500
+    inches_to_meters = 0.0254
+    meters_to_inches = 39.37008
+    heave_min = -1.2 * inches_to_meters
+    heave_max = 0.5 * inches_to_meters
+    angle_min = -1.3
+    angle_max = 1.3
+    heave_increment = (heave_max + abs(heave_min)) / (num_heaves - 1)
+    angle_increment = (angle_max + abs(angle_min)) / (num_angles - 1)
+
     iteration = 0
     for i in range(0, num_heaves):
         for j in range(0, num_angles):
             # chassis_heave = np.round(self.HEAVE_MIN + (i * heave_increment), 5)
             #             FRH: 4.95+${chassisHeaveScalar}
             # RRH: 5.73+(69.76*sin(${Chassis Angle}))+${chassisHeaveScalar}
-            chassis_heave = np.round(-1.2 + (i * heave_increment), 5)
-            chassis_angle = np.round(-1.3 + (j * angle_increment), 5)
-            frh = 0.12573 + chassis_heave
+            chassis_heave = np.round(heave_min + (i * heave_increment), 5)
+            chassis_angle = np.round(angle_min + (j * angle_increment), 5)
+            frh = (0.12573 + chassis_heave) * meters_to_inches
             rrh = (
                 0.145542
                 + (1.771904 * np.sin(chassis_angle * (np.pi / 180)))
                 + chassis_heave
+            ) * meters_to_inches
+            ride_height_combinations.loc[iteration, "Chassis Heave"] = (
+                chassis_heave * meters_to_inches
             )
-            ride_height_combinations.loc[iteration, "Chassis Heave"] = chassis_heave
             ride_height_combinations.loc[iteration, "Chassis Angle"] = chassis_angle
 
             ride_height_combinations.loc[iteration, "FRH"] = frh
@@ -47,10 +56,10 @@ def plot_ride_heights():
 
 
 def test_min_max(df):
-    print(df["FRH"].min())
-    print(df["FRH"].max())
-    print(df["RRH"].min())
-    print(df["RRH"].max())
+    print("FRH Min: ", df["FRH"].min())
+    print("FRH Max: ", df["FRH"].max())
+    print("RRH Min: ", df["RRH"].min())
+    print("RRH Max: ", df["RRH"].max())
 
 
 df = plot_ride_heights()
